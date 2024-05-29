1a0728ecece879bf8c2f9a1b408892676f843e88
vadimkriv <polenko1029@gmail.com>
2024-05-30 00:31:26 +0300

    first commit

---
+++ build/CMakeFiles/NeverLooseDeadline.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/NeverLooseDeadline.dir/main.cpp.o"
+  "CMakeFiles/NeverLooseDeadline.dir/main.cpp.o.d"
+  "NeverLooseDeadline"
+  "NeverLooseDeadline.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/NeverLooseDeadline.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ main.cpp
@@ -0,0 +1,15 @@
+
+    #include <iostream>
+
+    int main() {
+        int n;
+        std::cout << "Enter a number: ";
+        std::cin >> n;
+        
+        for (int i = 0; i < n; ++i) {
+            std::cout << "I will never lose deadline!" << std::endl;
+        }
+        
+        return 0;
+    }
+    
